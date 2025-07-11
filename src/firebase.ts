62b9ba0e06abd0b29e919edb8aa00031509cf541
LittleHero05 <isabellaporraslopez@gmail.com>
2025-07-11 15:47:08 -0400

    playing w firebse

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqSrpINb11lGJKMnuKOVbK5VwZ8acxdO0",
-  authDomain: "stride-cc49c.firebaseapp.com",
-  projectId: "stride-cc49c",
-  storageBucket: "stride-cc49c.firebasestorage.app",
-  messagingSenderId: "1042880771931",
-  appId: "1:1042880771931:web:a5822991f2629efbfa0c7e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-35JYW2KC18"
 };
 
